03e9f033f8bd03fec8c3e5eaf1e5fe697c51be18
Vincent Russell S. Ferrer <vincentrussell.ferrer.iics@ust.edu.ph>
2020-02-15 18:47:23 +0800

    commit

---
+++ js/homework.js
@@ -0,0 +1,22 @@
+/*Add the JavaScript here for the function billingFunction().
+  It is responsible for setting and clearing the fields in Billing Information */
+
+  
+function billingFunction() {
+    if (document.getElementById("same").checked) {
+            document.getElementById("billingName").value = document.getElementById("shippingName").value;
+    document.getElementById("billingZip").value = document.getElementById("shippingZip").value;
+    }
+    else {
+      document.getElementById("billingName").value = "";
+      document.getElementById("billingZip").value = "";
+    }
+      }
+
+      function sendtoServer() {
+        if (document.getElementById("veri").click) {
+
+          alert("The response was successfully delivered to the server");   
+        }
+
+      }
